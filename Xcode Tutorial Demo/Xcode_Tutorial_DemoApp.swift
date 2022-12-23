a378993b35d76d26eb7b380c3d45a00dbad2b7d1
SujeethJinesh <sujeethjinesh@gmail.com>
2022-12-23 15:55:48 -0800

    Initial Commit

---
+++ Xcode Tutorial Demo/Xcode_Tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_DemoApp.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Sujeeth Jinesh on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
